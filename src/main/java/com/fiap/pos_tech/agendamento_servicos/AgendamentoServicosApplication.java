97cf2c105a8dad67f51cb94072c42d29997748c0
Michael Barroso <michael_p._b.15600@hotmail.com>
2026-03-19 22:12:51 -0300

    feat: Initial commit

---
+++ src/main/java/com/fiap/pos_tech/agendamento_servicos/AgendamentoServicosApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.pos_tech.agendamento_servicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoServicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/pos_tech/agendamento_servicos/AgendamentoServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.pos_tech.agendamento_servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

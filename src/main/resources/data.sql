ae8541b3efb771104ace156765299ff1d87c93e0
Michael Barroso <michael_p._b.15600@hotmail.com>
2026-03-29 22:13:28 -0300

    feat: Atualização do properties por conta das informações do banco de dados e importação de dados default na base dados.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into cliente (id, cpf, email, nome)
+values ('55555555-5555-5555-5555-555555555555','32132354432','cliente1@gmail.com','Cliente1');
+
+insert into cliente (id, cpf, email, nome)
+values ('44444444-3333-4444-3333-444444444444','54356454355','cliente2@hotmail.com','Cliente2');

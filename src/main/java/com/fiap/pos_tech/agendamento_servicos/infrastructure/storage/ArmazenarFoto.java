8b6c5aeb2db8365cfd44c2396d3c10b9a2537528
Michael Barroso <michael_p._b.15600@hotmail.com>
2026-04-02 01:12:29 -0300

    style: Informações adicionais upload de fotos.

--- src/main/java/com/fiap/pos_tech/agendamento_servicos/infrastructure/storage/ArmazenarFoto.java
+++ src/main/java/com/fiap/pos_tech/agendamento_servicos/infrastructure/storage/ArmazenarFoto.java
@@ -8,6 +8,9 @@ public class ArmazenarFoto {
 
     public String uploadFile(MultipartFile file) {
         //Realiza o upload da foto para um serviço de armazenamento em nuvem.
+        //Foto não foi salva no banco de dados, pois isso influência grandemente no desempenho no banco de dados, além de ser uma má prática.
+        //E não foi implementado o serviço em nuvem para salvar o arquivo,
+        // pois isso não fazia parte do escopo do projeto nem dos temas da fase 3 da pós-tech.
 
         return "{URL de exemplo}?filename=" + file.getOriginalFilename();
     }
